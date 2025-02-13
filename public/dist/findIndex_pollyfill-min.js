d1a8f5fc24ef43c02d2c66699837b47cb02b8214
billpope <epop.bill@gmail.com>
2025-02-13 18:49:03 -0500

    delete

--- public/dist/findIndex_pollyfill-min.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";Array.prototype.findIndex=Array.prototype.findIndex||function(r){if(null===this)throw new TypeError("Array.prototype.findIndex called on null or undefined");if("function"!=typeof r)throw new TypeError("callback must be a function");for(var n=Object(this),t=n.length>>>0,e=arguments[1],o=0;o<t;o++)if(r.call(e,n[o],o,n))return o;return-1}})();
